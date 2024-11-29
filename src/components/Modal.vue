6bee09b49e30e78ecbf1187668468a6ed914aafc
김범수 <k1014ttl@naver.com>
2024-11-29 11:33:21 +0900

    first commit with vue3.js

---
+++ src/components/Modal.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="modal-overlay" @click.self="$emit('close')">
+            <slot></slot>
+    </div>
+</template>
+
+<script>
+export {};
+</script>
+
+<style scoped>
+.modal-overlay {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    padding: 20px;
+}
+</style>
